7db9711f905126686377da590d65267cc32dbe4a
Andy Hu <andy.hu@starfivetech.com>
2024-09-18 21:36:01 +0800

    genimage.sh: use genimage tool path under buildroot_rootfs
    Signed-off-by: Andy Hu <andy.hu@starfivetech.com>

--- genimage.sh
+++ genimage.sh
@@ -29,7 +29,7 @@ if [ $HWBOARD == "visionfive2" ]; then
 else
 	GENIMAGE_CFG=$TOPDIR/conf/genimage.cfg
 fi
-GENIMAGE=$TOPDIR/work/buildroot_initramfs/host/bin/genimage
+GENIMAGE=$TOPDIR/work/buildroot_rootfs/host/bin/genimage
 
 if [ ! -f $GENIMAGE ]; then
     printf $COLOR_RED
